0f90f2c460434e81e3445ef0b95a3eec52d5b048
952355188@qq.com <952355188@qq.com>
2022-11-29 16:47:58 +0100

    update client

--- client.go
+++ client.go
@@ -24,32 +24,60 @@ var (
 	servers     []gRPC.AuctionClient
 )
 
-func CheckServer(err error, serverId int) bool {
-	if err != nil {
-		log.Printf("Server %v unresponsive, connection disconnected...", serverId)
-		servers[serverId] = nil
-		chanDone[serverId] <- true
-		return false
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	args := os.Args[1:]
+	aid, _ := strconv.ParseInt(args[0], 10, 32)
+	id = int32(aid)
+	serverCount, _ := strconv.ParseInt(args[1], 10, 32)
+	bidCount, _ := strconv.ParseInt(args[2], 10, 32)
+	chanDone = make([]chan bool, int(serverCount))
+	servers = make([]gRPC.AuctionClient, int(serverCount))
+
+	for i := 0; i < int(serverCount); i++ {
+		go ConnServer(i)
 	}
-	return true
+
+	for i := 0; i < int(bidCount); i++ {
+		log.Printf("Bidding round %d/%d", i+1, int(bidCount))
+		Client(int(serverCount))
+	}
 }
+
+// func CheckServer(err error, serverId int) bool {
+// 	if err != nil {
+// 		log.Printf("Server %d disconnected", serverId)
+// 		servers[serverId] = nil
+// 		chanDone[serverId] <- true
+// 		return false
+// 	}
+// 	return true
+// }
 
 func BroadcastBid(amount int32) {
 	lamport++
 	timeout, _ := context.WithTimeout(context.Background(), time.Second*5)
 	currBid := gRPC.BidRequest{ClientId: id, Amount: amount, Lamport: lamport}
-	log.Printf("Bidding amount %d", amount)
+	log.Printf("Client %v bidding %v", id, amount)
 	for i, s := range servers {
 		if s != nil {
 			ack, err := s.Bid(timeout, &currBid)
-			if CheckServer(err, i) {
-				switch ack.Ack {
-				case gRPC.Acks_ACK_FAIL:
-					log.Printf("Bidding server %v failed!", i)
-				case gRPC.Acks_ACK_SUCCESS:
-					log.Printf("Bidding server %v sucess!", i)
-				case gRPC.Acks_ACK_EXCEPTION:
-					log.Printf("Bidding server %v exception", i)
+
+			check := true
+			if err != nil {
+				log.Printf("Server %d disconnected", i)
+				servers[i] = nil
+				chanDone[i] <- true
+				check = false
+			}
+
+			if check {
+				if ack.Ack == gRPC.Acks_ACK_SUCCESS {
+					log.Printf("Server %d accepted bid", i)
+				} else if ack.Ack == gRPC.Acks_ACK_FAIL {
+					log.Printf("Server %d rejected bid", i)
+				} else if ack.Ack == gRPC.Acks_ACK_EXCEPTION {
+					log.Printf("Server %d exception", i)
 				}
 			}
 		}
@@ -62,7 +90,14 @@ func GetResult() *gRPC.Outcome {
 	for i := 0; i < serverCount; i++ {
 		if servers[i] != nil {
 			result, err := servers[i].Result(timeout, &gRPC.Empty{})
-			if !CheckServer(err, i) {
+			check := true
+			if err != nil {
+				log.Printf("Server %d disconnected", i)
+				servers[i] = nil
+				chanDone[i] <- true
+				check = false
+			}
+			if !check {
 				continue
 			}
 			return result
@@ -71,7 +106,8 @@ func GetResult() *gRPC.Outcome {
 	return nil
 }
 
-func FrontEnd(servers int) {
+// Client: Front end of the client
+func Client(servers int) {
 	for {
 		if serverCount == servers {
 			result := GetResult()
@@ -80,7 +116,7 @@ func FrontEnd(servers int) {
 				if !roundOver {
 					roundOver = true
 					currentBid = 0
-					log.Printf("Round over, total bidding amount: %v", result.HighestPrice)
+					log.Printf("Round over, bid state: %s, total bidding amount: %v", result.BidState, result.HighestPrice)
 					return
 				}
 				time.Sleep(time.Second * time.Duration(rand.Intn(2)+1))
@@ -88,7 +124,7 @@ func FrontEnd(servers int) {
 			}
 
 			roundOver = false
-			currentBid += rand.Intn(500) + 1
+			currentBid += rand.Intn(50) + 1
 			BroadcastBid(int32(currentBid) + result.HighestPrice)
 			time.Sleep(time.Second * time.Duration(rand.Intn(2)+1))
 		}
@@ -96,36 +132,17 @@ func FrontEnd(servers int) {
 }
 
 // DialServer: Dial server and connect to it
-func DialServer(serverId int) {
+func ConnServer(serverId int) {
 	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", serverId+5000), grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("Failed to connect: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 
 	servers[serverId] = gRPC.NewAuctionClient(conn)
 	chanDone[serverId] = make(chan bool)
-	log.Printf("Client connected to server...")
+	log.Printf("Client %v bidding...", id)
 	serverCount++
 
 	<-chanDone[serverId]
 	conn.Close()
-}
-func main() {
-	rand.Seed(time.Now().UnixNano())
-	args := os.Args[1:]
-	aid, _ := strconv.ParseInt(args[0], 10, 32)
-	sc, _ := strconv.ParseInt(args[1], 10, 32)
-	br, _ := strconv.ParseInt(args[2], 10, 32)
-	id = int32(aid)
-	chanDone = make([]chan bool, int(sc))
-	servers = make([]gRPC.AuctionClient, int(sc))
-
-	for i := 0; i < int(sc); i++ {
-		go DialServer(i)
-	}
-
-	for i := 0; i < int(br); i++ {
-		log.Printf("Starting bidding round %d/%d", i+1, int(br))
-		FrontEnd(int(sc))
-	}
 }
